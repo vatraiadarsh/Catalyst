ed56acd1a14aa3d22d9b62f7a50cb99c46d0550a
Adarsha <vatraiadarsh@gmail.com>
2022-07-07 22:55:08 +1000

    reading user input

--- app.php
+++ app.php
@@ -1,1 +1,15 @@
 <?php
+
+function add_csv_file_to_database($file_name){
+
+    $csv_file = fopen($file_name, 'r');
+    $csv_file_data = fgetcsv($csv_file);
+
+    print_r($csv_file_data);
+
+    
+
+
+}
+
+add_csv_file_to_database('users.csv');
--- manage.bat
+++ manage.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "added barch file"
+git commit -m "reading user input "
 git push -u origin main
